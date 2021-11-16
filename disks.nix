3e6c5d32a15037fba5b8674dcda4b08a66e08072
Nathan Fish <lordcirth@gmail.com>
2021-11-16 14:09:26 -0500

    bcachefs

--- disks.nix
+++ disks.nix
@@ -1,14 +1,15 @@
 { config, pkgs, ... }: {
   environment.systemPackages = with pkgs; [ bcachefs-tools ];
+  boot.supportedFilesystems = [ "zfs" "bcachefs" ];
 
   fileSystems = {
     "/boot" = {
       device = /dev/sda1;
-      fsType = ext2;
+      fsType = "ext2";
     };
     "/" = {
       device = /dev/sda2;
-      fsType = bcachefs;
+      fsType = "bcachefs";
     };
   };
 }
